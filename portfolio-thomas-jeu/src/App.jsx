3a594e558850c4e89b9e6bf149ab73588b472ae3
thomasjeu <thomas.jeu@koredge.fr>
2023-08-10 15:50:16 +0200

    edit h1 card emplacement

--- portfolio-thomas-jeu/src/App.jsx
+++ portfolio-thomas-jeu/src/App.jsx
@@ -1,10 +1,9 @@
 import './style/App.scss'
-
 import React, {useEffect, useState} from "react";
-
+import {Routes, Route} from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import Loader from "./components/Loader.jsx";
-import {Routes, Route} from "react-router-dom";
+
 import HomePage from "./pages/Home.jsx";
 import Project from "./pages/Project.jsx";
 import NotFound from "./pages/NotFound.jsx";
@@ -25,8 +24,8 @@ function App() {
             {loading ? (<Loader />) : (<div></div>)}
             <Routes>
                 <Route exact path='/' element={<HomePage/>} />
-                <Route path='*' element={<NotFound/>} />
                 <Route exact path="/:projectName" element={<Project />} />
+                <Route path='*' element={<NotFound/>} />
             </Routes>
         </div>
     );
